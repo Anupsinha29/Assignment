a452710f23988c712ee5e390a999c16d66e86390
Anupsinha29 <anupsinhacse@gmail.com>
2022-08-12 23:34:17 +0530

    Assignment done

--- Assignment/src/main/java/Utility/JavaUtility.java
+++ Assignment/src/main/java/Utility/JavaUtility.java
@@ -1,5 +1,18 @@
 package Utility;
 
 public class JavaUtility {
-
+	
+	public long stringToLong(String timeout)
+	{
+		return Long.parseLong(timeout);
+	}
+	public double stringToDouble(String data)
+	{
+		return Double.parseDouble(data);
+	}
+	public float stringToFloat(String data)
+	{
+		return Float.parseFloat(data);
+	}
+  
 }
--- Assignment/src/main/java/objectRepository/PageObjectModel.java
+++ Assignment/src/main/java/objectRepository/PageObjectModel.java
@@ -1,11 +1,54 @@
 package objectRepository;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class PageObjectModel {
+	public WebDriver driver;
 	@FindBy(xpath="//input[@id='searchBox']")
 	private WebElement searchBox;
 	
+	private String cityName="//label[contains(.,'%s')]";
+	private String celsius="//div[contains(.,'%s')]/child::div/span[@class='tempRedText']";
+	private String farenheit="//div[contains(.,'%s')]/ancestor::div[@class=\"outerContainer\"]//div[@class='temperatureContainer']/span[@class='tempWhiteText']";
+	public PageObjectModel(WebDriver driver)
+	{
+		PageFactory.initElements(driver, this);
+		this.driver=driver;
 
+	}
+	
+	//business library
+	
+	public WebElement convertDynamicXpathToWebElement(String elementPartialXpath,String replaceData)
+	{
+		String xpath=String.format(elementPartialXpath, replaceData);
+		return driver.findElement(By.xpath(xpath));
+	}
+
+	public void selectTheCityName(String replaceData) {
+		WebElement city = convertDynamicXpathToWebElement(cityName, replaceData);
+		//Actions act=new Actions(driver);
+		//act.moveToElement(searchBox);
+		city.click();
+	}
+	public void enterCityName(String city)
+	{
+		searchBox.clear();
+		searchBox.sendKeys(city);
+	}
+	public String getTheValuesInCelsius(String replaceData)
+	{
+		WebElement celsiusValue=convertDynamicXpathToWebElement(celsius, replaceData);
+			return	celsiusValue.getText().trim();
+	}
+	public String getValuesInFarenheit(String replaceData)
+	{
+		WebElement farenheitValue = convertDynamicXpathToWebElement(farenheit, replaceData);
+		return farenheitValue.getText().trim();
+	}
+	
 }
